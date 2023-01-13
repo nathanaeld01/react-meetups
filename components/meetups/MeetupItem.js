284ef6212e95fd457e2e9d997b2a25ab291340ae
nathanaeld01 <71888935+nathanaeld01@users.noreply.github.com>
2023-01-13 14:05:13 +0530

    fixed item styling

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -11,7 +11,7 @@ function MeetupItem(props) {
 	};
 
 	return (
-		<li className={classes.item}>
+		<li className="item">
 			<Card>
 				<div className={classes.image}>
 					<img src={props.image} alt={props.title} />
