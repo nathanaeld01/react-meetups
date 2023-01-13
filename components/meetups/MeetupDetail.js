3e9d5faa75bb5230d6bea459e86951cf05dc146d
nathanaeld01 <71888935+nathanaeld01@users.noreply.github.com>
2023-01-13 14:54:12 +0530

    modified details component and styling

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,10 +3,14 @@ import classes from "./MeetupDetail.module.css";
 function MeetupDetail({ image, title, address, description }) {
     return (
         <section className={classes.detail}>
-            <img src={image} alt={title} />
-            <h1>{title}</h1>
-            <address>{address}</address>
-            <p>{description}</p>
+            <div className={classes.image}>
+                <img src={image} alt={title} />
+            </div>
+            <div className={classes.info}>
+                <h1>{title}</h1>
+                <address>{address}</address>
+                <p>{description}</p>
+            </div>
         </section>
     )
 };
